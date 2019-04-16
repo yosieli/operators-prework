5657e0f1430ceb2e2926e26dc10d24f7c012f0f9
feven <feventsegay6@gmail.com>
2019-04-16 17:18:55 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,12 +11,8 @@ end
 
 
 def not_safe?(speed)
-  if speed < 40 &&
-    return true
-  elsif speed > 60
-  return true
-  elsif speed<=60 && speed>=40
-    return false
+  if speed < 40 && speed > 60 ? true:false
+   
 end
 end
 	
