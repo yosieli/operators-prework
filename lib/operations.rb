36dd8691d63e844358951ec040a7db0ca109d098
feven <feventsegay6@gmail.com>
2019-04-16 17:29:35 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,19 @@
 def unsafe?(speed)
-
+ if speed > 60 
+   return true
+   elsif speed < 40
+   return true
+   elsif speed<=60 && speed>=40
+   return false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+  speed >= 40 && speed <=60 ?  false:  true
+   
+
 end
 	
 
